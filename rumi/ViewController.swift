8f07003ea0b5e1ea80766b937db6a9b462322c94
Saravanan R <saravananr@Saravanans-MacBook-Pro.local>
2017-09-04 16:57:46 +0530

    sas

--- rumi/ViewController.swift
+++ rumi/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("rumi3434343")
+        print("qq")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
