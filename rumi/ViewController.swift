83648ba20cf8a05a558224ce17cbfe7d8b6cc587
Saravanan R <saravananr@Saravanans-MacBook-Pro.local>
2017-09-04 15:31:43 +0530

    phase9

--- rumi/ViewController.swift
+++ rumi/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("rumijhk")
+        print("rumishams")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
