9c1f5ec51c3f72a7b1277ec8f3ca90bf17dc85c8
Saravanan R <saravananr@Saravanans-MacBook-Pro.local>
2017-09-05 13:10:41 +0530

    ff

--- rumi/ViewController.swift
+++ rumi/ViewController.swift
@@ -21,10 +21,7 @@ class ViewController: UIViewController {
         
          print("implementation")
 
-         print("branch")
-         print("branch2")
-        print("branch3")
-         print("branch4")
+    
 
         // Do any additional setup after loading the view, typically from a nib.
     }
