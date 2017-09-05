c9805869ced3920c244a54bd90afef0c8e123ca2
Saravanan R <saravananr@Saravanans-MacBook-Pro.local>
2017-09-05 13:37:19 +0530

    wde

--- rumi/ViewController.swift
+++ rumi/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         
 
         print("imple")
+        print("imple1")
     
 
         // Do any additional setup after loading the view, typically from a nib.
